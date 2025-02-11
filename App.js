61f87a27ac210fb82e147a050d47cdc199301ea0
Nilkanth Kulkarni <Nilkanth.mohan@Neutrinos.co>
2025-02-11 15:46:24 +0530

    EP03: component creation tutorial

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
+
 import React from "react";
-import ReactDOM from 'react-dom/client';
-const parent = React.createElement(
-  "div",
-  { id: "main" },
-  React.createElement(
-    "div",
-    { id: "children" },
-    React.createElement("p", {}, "This is the First app with React.")
-  )
-);
+import ReactDOM from "react-dom/client";
+const ElementComponent = () => {
+
+    console.log("this value from arrow function", this)
+  return (
+    <p>
+      This is the paragraph from another component nested inside heading
+      component
+    </p>
+  );
+};
+
+const FirstComponent = function(){
+    
+    console.log("this value from normal function", this)
+  return (
+    <div id="container">
+      
+      <h1>
+        This is first ever component created by you, good going keep it up!!
+      </h1>
+      <ElementComponent />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<FirstComponent />);
